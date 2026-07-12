97820ac6c3aaa8f094346be4af7485a8dbf27c4e
Surayyo <rashidovas1995@gmail.com>
2026-07-13 01:34:20 +0500

    fix: adminkada status log korinishi

--- apps/samples/signals.py
+++ apps/samples/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils import timezone
 
-from .models import Sample
+from .models import Sample, SampleStatusLog
 
 
 @receiver(pre_save, sender=Sample)
@@ -15,3 +15,22 @@ def generate_sample_id(sender, instance, **kwargs):
         ).order_by('-id').first()
         new_num = int(last.sample_id.split('-')[-1]) + 1 if last else 1
         instance.sample_id = f'CHEM-{year}-{new_num:05d}'
+
+
+@receiver(pre_save, sender=Sample)
+def log_status_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        old = Sample.objects.get(pk=instance.pk)
+    except Sample.DoesNotExist:
+        return
+
+    if old.status != instance.status:
+        SampleStatusLog.objects.create(
+            sample=instance,
+            old_status=old.status,
+            new_status=instance.status,
+            changed_by=None,
+        )
